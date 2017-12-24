57cc0712d3bb7145e0cb3def2721e76daa85ba2c
hades_zhao <hades_zhao@126.com>
2017-12-24 13:35:15 +0800

    init project

---
+++ news/app.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+#
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/shiyanlou'
+db = SQLAlchemy(app)
+
+
+class File(db.Model):
+    id = db.Column(db.Integer,primary_key=True,index=True)
+    title = db.Column(db.String(80),unique=True)
+    created_time = db.Column(db.DateTime,nullable=True)
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
+    category = db.relationship('Category',backref='files')
+    content = db.Column(db.Text)
+    
+    def __init__(self,title,created_time,category_id,content):
+        self.title = title
+        self.created_time = created_time
+        self.category_id = category_id
+        self.content = content
+    
+    def __repr__(self):
+        return '<File %r>' %self.title
+
+class Category(db.Model):
+    id = db.Column(db.Integer,primary_key=True,index=True)
+    name = db.Column(db.String(80))
+    
+    def __init__(self,name):
+       self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' %self.name 
+
+
+@app.route('/')
+def index():
+    db.create_all()
+    file_instance_list = File.query.all()
+    title_id_list = [(file_instance.title,file_instance.id) for \
+            file_instance in file_instance_list ]
+    return render_template('index.html',title_id_list=title_id_list)
+
+
+
+
+@app.route('/files/<file_id>')
+def file(file_id):
+    db.create_all()
+    file_instance = File.query.filter_by(id=file_id).first_or_404()
+    _category_instance = Category.query.filter_by(id=file_instance.category_id).first_or_404()
+    file_category = _category_instance.name
+    file_dict = { 'created_time':datetime.strftime(file_instance.created_time,'%Y-%m-%d %H:%M:%S'),'category':file_category,'content':file_instance.content }
+    return render_template('file.html',file_dict = file_dict)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'),404
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ news/shiyanlou.sql
@@ -0,0 +1,87 @@
+-- MySQL dump 10.13  Distrib 5.5.50, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: shiyanlou
+-- ------------------------------------------------------
+-- Server version	5.5.50-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `shiyanlou`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `shiyanlou` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `shiyanlou`;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Java'),(2,'Python');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `file`
+--
+
+DROP TABLE IF EXISTS `file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(80) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `content` text,
+  PRIMARY KEY (`id`),
+  KEY `fk_category` (`category_id`),
+  CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `file`
+--
+
+LOCK TABLES `file` WRITE;
+/*!40000 ALTER TABLE `file` DISABLE KEYS */;
+/*!40000 ALTER TABLE `file` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-23 13:18:33
